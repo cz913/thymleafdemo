83605465937cfef40700c13d4c539e32b01b003c
com-offcn <305152268@qq.com>
2020-10-22 16:12:48 +0800

    Initial commit

---
+++ src/main/java/com/oofcn/entity/User.java
@@ -0,0 +1,38 @@
+package com.oofcn.entity;
+
+/**
+ * @PackageName: com.oofcn.entity
+ * @ClassName: User
+ * @Description: 加油，宝贝
+ * @Author Orange
+ * @Date 2020/10/22 10:22
+ */
+public class User {
+    private Integer id;
+    private String name;
+    private int age;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
